b7f7460ba3adab3fe0bf98f83a42585c5ce0bbd2
Suraj <Suraj@Tinas-MacBook-Air.local>
2026-06-11 20:19:45 +0530

    Add Meta Pixel tracking

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,6 +6,8 @@ export const metadata = {
   description: "Premium Astrology Platform",
 };
 
+const FB_PIXEL_ID = "1685249759459324";
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
@@ -17,6 +19,31 @@ export default function RootLayout({ children }) {
       >
         {children}
 
+        {/* Meta Pixel */}
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;
+            n.push=n;
+            n.loaded=!0;
+            n.version='2.0';
+            n.queue=[];
+            t=b.createElement(e);
+            t.async=!0;
+            t.src=v;
+            s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s);
+            }(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+
+            fbq('init', '${FB_PIXEL_ID}');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+
+        {/* Google Analytics */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-EYJL0RXSV6"
           strategy="afterInteractive"

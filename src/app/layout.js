5f8b5e2fa50ad2ef541554302229184d79b928ba
Suraj <Suraj@Tinas-MacBook-Air.local>
2026-06-11 16:04:44 +0530

    Added Google Analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import Script from "next/script";
 
 export const metadata = {
   title: "Vedmantra",
@@ -15,6 +16,21 @@ export default function RootLayout({ children }) {
         }}
       >
         {children}
+
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-EYJL0RXSV6"
+          strategy="afterInteractive"
+        />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-EYJL0RXSV6');
+          `}
+        </Script>
       </body>
     </html>
   );
